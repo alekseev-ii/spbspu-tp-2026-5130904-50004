d76bde0aadc6932187e48d0d5edcbfcad0f600a5
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-05-19 14:57:38 +0300

    feat(main): main v1.0

--- alekseev.ivan/T2/main.cpp
+++ alekseev.ivan/T2/main.cpp
@@ -1,1 +1,14 @@
-int main();
+#include <vector>
+#include "DataStruct.h"
+#include <iterator>
+#include <algorithm>
+
+int main()
+{
+  std::vector< alekseev::DataStruct > data;
+  std::istream_iterator< alekseev::DataStruct > begin(std::cin), end{};
+  std::copy(begin, end, std::back_inserter(data));
+  std::sort(data.begin(), data.end());
+  std::ostream_iterator< alekseev::DataStruct > out(std::cout, "\n");
+  std::copy(data.begin(), data.end(), out);
+}
